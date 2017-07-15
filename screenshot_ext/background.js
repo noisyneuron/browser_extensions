6e662e66a853828bbb2631c394c5cf686cd320e5
sukanya <sukanya.aneja@gmail.com>
2017-07-15 10:31:48 -0400

    clickity msg

--- screenshot_ext/background.js
+++ screenshot_ext/background.js
@@ -3,21 +3,23 @@
 var portFromCS;
 
 function connected(p) {
-    portFromCS = p;
+   portFromCS = p;
 }
 
 browser.runtime.onConnect.addListener(connected);
 
 function onCaptured(imageUri) {
-      console.log(imageUri);
-      portFromCS.postMessage({"data": imageUri});
+    console.log(imageUri);
+    portFromCS.postMessage({"data": imageUri});
 }
 
 function onError(error) {
-      console.log(`Error: ${error}`);
+    console.log(`Error: ${error}`);
 }
 
-browser.browserAction.onClicked.addListener(function() {
-      var capturing = browser.tabs.captureVisibleTab();
-      capturing.then(onCaptured, onError);
+browser.runtime.onMessage.addListener(function(d) {
+   console.log(d);
+   var capturing = browser.tabs.captureVisibleTab();
+   capturing.then(onCaptured, onError);
 });
+
--- screenshot_ext/content.js
+++ screenshot_ext/content.js
@@ -1,8 +1,26 @@
 var myPort = browser.runtime.connect({name:"port-from-cs"});
 
-myPort.onMessage.addListener(function(m) {
+browser.runtime.onMessage.addListener(function(m) {
     // display the image!
       console.log("In content script, received message from background script: ");
       console.log(m);
+      // threejs script goes here
       document.getElementsByTagName('html')[0].innerHTML = '<img src="' + m.data + '" />'
 });
+
+var a_elements = document.getElementsByTagName('a');
+
+for(var i=0; i<a_elements.length; i++) {
+  a_elements[i].onclick = function(e) {
+    e.preventDefault();
+    browser.runtime.sendMessage({'link' : e.target.href})
+  }
+}
+
+// window.addEventListener("beforeunload", function(e) {
+//   console.log("unloading");
+//   browser.runtime.sendMessage({"data":"MOO"});
+//   setTimeout(function() {
+//     e.returnValue = "\o/";
+//   }, 2000);
+// })
